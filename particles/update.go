14a975b5656c58545c4ddb1e221b42f85a0e5d67
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2022-12-14 19:08:32 +0100

    Add into update

--- particles/update.go
+++ particles/update.go
@@ -12,9 +12,9 @@ import (
 // projet.
 // C'est à vous de développer cette fonction.
 func (s *System) Update() {
-	for i:=s.Content.Front(); i != nil; i = i.Next(){
+	for i := s.Content.Front(); i != nil; i = i.Next() {
 		p, ok := i.Value.(*Particle)
-		if ok{
+		if ok {
 			p.PositionX = p.PositionX + p.SpeedX
 			p.PositionY = p.PositionY + p.SpeedY
 		}
@@ -23,14 +23,27 @@ func (s *System) Update() {
 	var _PositionX float64
 	var _PositionY float64
 	rand.Seed(time.Now().UnixNano())
-	for i:=0;float64(i)<config.General.SpawnRate;i++{
+	for i := 0; float64(i) < config.General.SpawnRate; i++ {
 
-		if config.General.RandomSpawn{
+		if config.General.RandomSpawn {
 			_PositionX = float64(rand.Intn(config.General.WindowSizeX)) + rand.Float64()
 			_PositionY = float64(rand.Intn(config.General.WindowSizeY)) + rand.Float64()
-		}else{
+		} else {
 			_PositionX = float64(config.General.SpawnX)
 			_PositionY = float64(config.General.SpawnY)
+		}
+		// Initialisation de la vitesse de la particule
+		var minSpeed, maxSpeed int
+		switch config.General.SpeedType {
+		case 1:
+			minSpeed = 0
+			maxSpeed = 1
+		case 2:
+			minSpeed = 1
+			maxSpeed = 3
+		case 3:
+			minSpeed = 3
+			maxSpeed = 5
 		}
 		s.Content.PushFront(&Particle{
 			PositionX: _PositionX,
@@ -38,8 +51,8 @@ func (s *System) Update() {
 			ScaleX:    config.General.ScaleX, ScaleY: config.General.ScaleY,
 			ColorRed: config.General.ColorRed, ColorGreen: config.General.ColorGreen, ColorBlue: config.General.ColorBlue,
 			Opacity: config.General.Opacity,
-			SpeedX : float64(rand.Intn(2)) + rand.Float64() - 1,
-			SpeedY : float64(rand.Intn(2)) + rand.Float64() - 1,
+			SpeedX:  (float64(rand.Intn(maxSpeed-minSpeed)+minSpeed) + rand.Float64()) * float64(rand.Intn(2)*2-1),
+			SpeedY:  (float64(rand.Intn(maxSpeed-minSpeed)+minSpeed) + rand.Float64()) * float64(rand.Intn(2)*2-1),
 		})
 	}
 
