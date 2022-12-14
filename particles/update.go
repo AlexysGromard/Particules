a61aac598f9bd23c694d07e3035961d8394df74f
Floran <E226752U>
2022-12-14 17:03:11 +0100

    Modification Update mouvement

--- particles/update.go
+++ particles/update.go
@@ -1,8 +1,19 @@
 package particles
 
+//import "../project-particles/particles"
+
 // Update mets à jour l'état du système de particules (c'est-à-dire l'état de
 // chacune des particules) à chaque pas de temps. Elle est appellée exactement
 // 60 fois par seconde (de manière régulière) par la fonction principale du
 // projet.
 // C'est à vous de développer cette fonction.
-func (s *System) Update() {}
+func (s *System) Update() {
+	for i:=s.Content.Front(); i != nil; i = i.Next(){
+
+		p, ok := i.Value.(*Particle)
+		if ok{
+			p.PositionX = p.PositionX + p.SpeedX
+			p.PositionY = p.PositionY + p.SpeedY
+		}
+	}
+}
