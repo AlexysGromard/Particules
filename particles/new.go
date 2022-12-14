5242b0d6f083fa64a7abca702f2b6fc332fc57fe
Floran <E226752U>
2022-12-14 16:00:34 +0100

    4.1 New System()

--- particles/new.go
+++ particles/new.go
@@ -3,6 +3,8 @@ package particles
 import (
 	"container/list"
 	"project-particles/config"
+	"math/rand"
+	"time"
 )
 
 // NewSystem est une fonction qui initialise un système de particules et le
@@ -12,12 +14,40 @@ import (
 // centre de l'écran.
 func NewSystem() System {
 	l := list.New()
-	l.PushFront(&Particle{
-		PositionX: float64(config.General.WindowSizeX) / 2,
-		PositionY: float64(config.General.WindowSizeY) / 2,
-		ScaleX:    1, ScaleY: 1,
-		ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
-		Opacity: 1,
-	})
+
+	/* //créer une particule //
+				l.PushFront(&Particle{
+				PositionX: float64(config.General.WindowSizeX) / 2,
+				PositionY: float64(config.General.WindowSizeY) / 2,
+				ScaleX:    1, ScaleY: 1,
+				ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
+				Opacity: 1,
+			}) 
+	*/
+
+	var _PositionX float64
+	var _PositionY float64
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i:=0;i < config.General.InitNumParticles ; i++ {
+
+		if config.General.RandomSpawn{
+			_PositionX = float64(rand.Intn(config.General.WindowSizeX)) + rand.Float64()
+			_PositionY = float64(rand.Intn(config.General.WindowSizeY)) + rand.Float64()
+		}else{
+			_PositionX = float64(config.General.SpawnX)
+			_PositionY = float64(config.General.SpawnY)
+		}
+
+		l.PushFront(&Particle{
+			PositionX: _PositionX,
+			PositionY: _PositionY,
+			ScaleX:    1, ScaleY: 1,
+			ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
+			Opacity: 1,
+		})
+	}
+	
 	return System{Content: l}
 }
