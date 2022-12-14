8d3cd2bccbb6064bc9423085cfaa7c912915bedd
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2022-12-14 18:42:39 +0100

    Fixed speed bug

--- particles/new.go
+++ particles/new.go
@@ -2,10 +2,9 @@ package particles
 
 import (
 	"container/list"
-	"project-particles/config"
 	"math/rand"
+	"project-particles/config"
 	"time"
-	"fmt"
 )
 
 // NewSystem est une fonction qui initialise un système de particules et le
@@ -17,13 +16,13 @@ func NewSystem() System {
 	l := list.New()
 
 	/* //créer une particule //
-				l.PushFront(&Particle{
-				PositionX: float64(config.General.WindowSizeX) / 2,
-				PositionY: float64(config.General.WindowSizeY) / 2,
-				ScaleX:    1, ScaleY: 1,
-				ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
-				Opacity: 1,
-			}) 
+		l.PushFront(&Particle{
+		PositionX: float64(config.General.WindowSizeX) / 2,
+		PositionY: float64(config.General.WindowSizeY) / 2,
+		ScaleX:    1, ScaleY: 1,
+		ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
+		Opacity: 1,
+	})
 	*/
 
 	var _PositionX float64
@@ -31,12 +30,12 @@ func NewSystem() System {
 
 	rand.Seed(time.Now().UnixNano())
 
-	for i:=0;i < config.General.InitNumParticles ; i++ {
+	for i := 0; i < config.General.InitNumParticles; i++ {
 		// Initialisation de la position de la particule
-		if config.General.RandomSpawn{
+		if config.General.RandomSpawn {
 			_PositionX = float64(rand.Intn(config.General.WindowSizeX)) + rand.Float64()
 			_PositionY = float64(rand.Intn(config.General.WindowSizeY)) + rand.Float64()
-		}else{
+		} else {
 			_PositionX = float64(config.General.SpawnX)
 			_PositionY = float64(config.General.SpawnY)
 		}
@@ -53,19 +52,17 @@ func NewSystem() System {
 			minSpeed = 3
 			maxSpeed = 5
 		}
+
 		l.PushFront(&Particle{
 			PositionX: _PositionX,
 			PositionY: _PositionY,
 			ScaleX:    config.General.ScaleX, ScaleY: config.General.ScaleY,
 			ColorRed: config.General.ColorRed, ColorGreen: config.General.ColorGreen, ColorBlue: config.General.ColorBlue,
 			Opacity: config.General.Opacity,
-			SpeedX : float64(rand.Intn(maxSpeed*2-minSpeed)+minSpeed) + rand.Float64() - 10,
-			SpeedY : float64(rand.Intn(maxSpeed*2-minSpeed)+minSpeed) + rand.Float64() - 10,
+			SpeedX:  (float64(rand.Intn(maxSpeed-minSpeed)+minSpeed) + rand.Float64()) * float64(rand.Intn(2)*2-1),
+			SpeedY:  (float64(rand.Intn(maxSpeed-minSpeed)+minSpeed) + rand.Float64()) * float64(rand.Intn(2)*2-1),
 		})
 	}
 
-	
-
-	
 	return System{Content: l}
 }
