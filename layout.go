cd1c8ea59e68b157113d549af2b20d5f61b0beea
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2022-12-31 17:24:23 +0100

    WindowResizing

--- layout.go
+++ layout.go
@@ -1,8 +1,12 @@
 package main
 
+import (
+	"project-particles/config"
+)
+
 // Layout définit la taille en pixels de la zone d'affichage des particules
 // en fonction de la taille en pixels de la fenêtre. Vous n'avez jamais à
 // modifier cette fonction.
 func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return outsideWidth, outsideHeight
+	return config.General.WindowSizeX, config.General.WindowSizeY
 }
