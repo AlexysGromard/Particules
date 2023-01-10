547cb44c95aaa42263088cb7ad2b31bcfd22634d
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2023-01-10 15:13:43 +0100

    ReadImages for config page

--- draw.go
+++ draw.go
@@ -18,21 +18,18 @@ const (
 
 var CurrentPage int
 
-// Button
-
 // Draw se charge d'afficher à l'écran l'état actuel du système de particules
 // g.system. Elle est appelée automatiquement environ 60 fois par seconde par
 // la bibliothèque Ebiten. Cette fonction pourra être légèrement modifiée quand
 // c'est précisé dans le sujet.
 func (g *game) Draw(screen *ebiten.Image) {
 	if CurrentPage == configurationsPage {
-		// Affichage de la page de configuration
-		// Print text in center of Page en gros
+
 	} else if CurrentPage == particlesPage {
 		for e := g.system.Content.Front(); e != nil; e = e.Next() {
 			p, ok := e.Value.(*particles.Particle)
 			if ok {
-				if !((p.PositionX < 0-config.General.MarginOutsideScreen || p.PositionX > float64(config.General.WindowSizeX)+config.General.MarginOutsideScreen) || (p.PositionY < 0-config.General.MarginOutsideScreen || p.PositionY > float64(config.General.WindowSizeY)+config.General.MarginOutsideScreen) || p.Life <= 0){
+				if !((p.PositionX < 0-config.General.MarginOutsideScreen || p.PositionX > float64(config.General.WindowSizeX)+config.General.MarginOutsideScreen) || (p.PositionY < 0-config.General.MarginOutsideScreen || p.PositionY > float64(config.General.WindowSizeY)+config.General.MarginOutsideScreen) || p.Life <= 0) {
 					options := ebiten.DrawImageOptions{}
 					options.GeoM.Rotate(p.Rotation)
 					options.GeoM.Scale(p.ScaleX, p.ScaleY)
