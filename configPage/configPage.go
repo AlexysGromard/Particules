9337b13bde87cbb569078ee799fec69dc97a130a
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2023-01-11 18:12:45 +0100

    Commentaires et optimisation

--- configPage/configPage.go
+++ configPage/configPage.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/image/font"
 )
 
+// Création de la structure Button
 type Button struct {
 	x, y, width, height int
 	imageNormal         *ebiten.Image
@@ -22,6 +23,7 @@ type Button struct {
 	onClick             func()
 }
 
+// Création de la structure Checkbox
 type Checkbox struct {
 	x, y, width, height int
 	imageNormal         *ebiten.Image
@@ -35,6 +37,8 @@ type Checkbox struct {
 	onClick             func()
 }
 
+// Impression des bouttons
+// Cette fonction met à jour l'affichage des bouttons en fonction de leur état (hover, pressed)
 func (b *Button) Draw(screen *ebiten.Image) {
 	var img *ebiten.Image
 	if b.pressed {
@@ -49,6 +53,8 @@ func (b *Button) Draw(screen *ebiten.Image) {
 	screen.DrawImage(img, opt)
 }
 
+// Impression des Checkbox
+// Cette fonction met à jour l'affichage des checkbox en fonction de leur état (hover, pressed, checked)
 func (c *Checkbox) Draw(screen *ebiten.Image) {
 	var img *ebiten.Image
 	if c.hover {
@@ -61,9 +67,14 @@ func (c *Checkbox) Draw(screen *ebiten.Image) {
 	screen.DrawImage(img, opt)
 }
 
+// Mise à jour des bouttons
+// Cette fonction regarde si la souris est sur un boutton et si elle est enfoncée
+// Elle regarde vérifie si le bouton en bas à droite est toujours à la bonne position (en fonction de l'affichage de la fenêtre)
 func (b *Button) updateButton(screen *ebiten.Image) {
+	// Position de la souris
 	x, y := ebiten.CursorPosition()
 	b.hover = false
+	// Si la souris est sur le boutton
 	if x > b.x && x < b.x+b.width && y > b.y && y < b.y+b.height {
 		b.hover = true
 		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
@@ -72,15 +83,22 @@ func (b *Button) updateButton(screen *ebiten.Image) {
 	} else {
 		b.pressed = false
 	}
+	// Si le bouton en bas à gauche n'est plus à la bonne place en fonction de la taille d'écran
 	if b.x < config.General.WindowSizeX-b.width || b.x > config.General.WindowSizeX-b.width+10 || b.y < config.General.WindowSizeY-b.height || b.y > config.General.WindowSizeY-b.height+10 {
 		b.x = config.General.WindowSizeX - 100 - 10
 		b.y = config.General.WindowSizeY - 50 - 10
 	}
 }
 
+// Mise à jour des checkbox
+// Cette fonction regarde si la souris est sur une checkbox et si elle est enfoncée
+// Elle regarde vérifie si la checkbox est cochée ou non
+// Si la checkbox est cochée, elle affiche l'image
 func (c *Checkbox) updateCheckbox(screen *ebiten.Image) {
+	// Position de la souris
 	x, y := ebiten.CursorPosition()
 	c.hover = false
+	// Si la souris est sur la checkbox
 	if x > c.x && x < c.x+c.width && y > c.y && y < c.y+c.height {
 		c.hover = true
 		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
@@ -95,11 +113,14 @@ func (c *Checkbox) updateCheckbox(screen *ebiten.Image) {
 
 		}
 	}
+	// Si la checkbox est cochée
 	if c.checked {
+		// Afficher l'image de droite
 		opt := &ebiten.DrawImageOptions{}
 		opt.GeoM.Translate(float64(c.x), float64(c.y))
 		screen.DrawImage(c.circleTrue, opt)
 	} else {
+		// Afficher l'image de gauche
 		opt := &ebiten.DrawImageOptions{}
 		opt.GeoM.Translate(float64(c.x), float64(c.y))
 		screen.DrawImage(c.circleFalse, opt)
@@ -107,6 +128,8 @@ func (c *Checkbox) updateCheckbox(screen *ebiten.Image) {
 
 }
 
+// NewButton crée un nouveau boutton en fonction des paramètres
+// Entrées : x,y (position), width, height (taille), images (liste des images), text (texte du boutton), font (police d'écriture), onClick (fonction à appeler quand on clique sur le boutton)
 func newButton(x, y, width, height int, images []*ebiten.Image, text string, font font.Face, onClick func()) *Button {
 	return &Button{
 		x:            x,
@@ -124,6 +147,8 @@ func newButton(x, y, width, height int, images []*ebiten.Image, text string, fon
 	}
 }
 
+// NewCheckbox crée une nouvelle checkbox en fonction des paramètres
+// Entrées : x,y (position), width, height (taille), images (liste des images), checked (état de la checkbox), onClick (fonction à appeler quand on clique sur la checkbox)
 func newCheckbox(x, y, width, height int, images []*ebiten.Image, checked bool, onClick func()) *Checkbox {
 	return &Checkbox{
 		x:           x,
@@ -140,8 +165,8 @@ func newCheckbox(x, y, width, height int, images []*ebiten.Image, checked bool, 
 }
 
 var (
-	button   *Button
-	checkbox *Checkbox
+	accessParticlesButton *Button
+	checkbox              *Checkbox
 )
 
 // FidnImage cherche une image grâce à son nom dans la liste des images
@@ -157,22 +182,24 @@ func findImage(images []Images, filename string) *ebiten.Image {
 	return nil
 }
 
+// UpdateConfigPage est la fonction qui est appelée pour mettre à jour la page de configuration
 func UpdateConfigPage(screen *ebiten.Image) error {
 	fontFace, _ := loadFont(fontFaceRegular, 20)
-	// Initialize button if it hasn't been created yet
-	if button == nil {
+	// Crée le boutton si il n'existe pas
+	if accessParticlesButton == nil {
 		images := []*ebiten.Image{findImage(ImageList, "button-idle.png"), findImage(ImageList, "button-pressed.png"), findImage(ImageList, "button-hover.png")}
-		button = newButton(config.General.WindowSizeX-100-10, config.General.WindowSizeY-50-10, 100, 50, images, "Hello World", fontFace, func() { fmt.Println("test") })
+		accessParticlesButton = newButton(config.General.WindowSizeX-100-10, config.General.WindowSizeY-50-10, 100, 50, images, "Hello World", fontFace, func() { fmt.Println("test") })
 	}
-	// Update the button's state
-	button.updateButton(screen)
-	// Draw the button
-	button.Draw(screen)
-	// Create checkbox if it hasn't been created yet
+	// Met à jour l'état du bouton et l'affiche
+	accessParticlesButton.updateButton(screen)
+	accessParticlesButton.Draw(screen)
+
+	// Crée la checkbox si elle n'existe pas
 	if checkbox == nil {
 		images := []*ebiten.Image{findImage(ImageList, "checkbox-idle.png"), findImage(ImageList, "checkbox-hover.png"), findImage(ImageList, "checkbox-checked-idle.png"), findImage(ImageList, "checkbox-unchecked-idle.png")}
 		checkbox = newCheckbox(10, 70, 50, 50, images, config.General.Debug, func() { config.General.Debug = !config.General.Debug })
 	}
+	// Met à jour l'état de la checkbox et l'affiche
 	checkbox.updateCheckbox(screen)
 	checkbox.Draw(screen)
 
