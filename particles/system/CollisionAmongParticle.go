06e8c561c22220f98e3951001a55b6e0631e3154
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2023-01-18 17:36:23 +0100

    Comments CollisionAmongParticle.go

--- particles/system/CollisionAmongParticle.go
+++ particles/system/CollisionAmongParticle.go
@@ -6,6 +6,11 @@ import (
 	"project-particles/particles"
 )
 
+// La fonction CollisionAmongParticle fait et vérifie les collisions entre les particules
+// Elle prend en paramètre un élément de la liste des particules et une particule
+// Elle vérifie si les particules sont assez proche pour qu'il y est une collision
+// Si il y a une collision elle échange les vitesses des particules
+// et les éloigne un peu l'une de l'autre
 func CollisionAmongParticle(ElementActuel *list.Element, ParticuleActuelle *particles.Particle) {
 
 	for ElementProche := ElementActuel.Next(); ElementProche != nil; ElementProche = ElementProche.Next() {
@@ -17,7 +22,7 @@ func CollisionAmongParticle(ElementActuel *list.Element, ParticuleActuelle *part
 					ParticuleProche.SpeedX, ParticuleProche.SpeedY, ParticuleActuelle.SpeedX, ParticuleActuelle.SpeedY = ParticuleActuelle.SpeedX, ParticuleActuelle.SpeedY, ParticuleProche.SpeedX, ParticuleProche.SpeedY
 					// pour éviter que des particules ne se retrouve collée on les éloigne un peu l'une de l'autre
 					angle1 := math.Atan2(ParticuleActuelle.PositionY-ParticuleProche.PositionY, ParticuleActuelle.PositionX-ParticuleProche.PositionX)
-
+					// on ajoute un peu de vitesse pour que les particules ne se retrouve pas collée
 					ParticuleActuelle.PositionX = ParticuleActuelle.PositionX + math.Cos(angle1)*ParticuleActuelle.ScaleX*0.1
 					ParticuleActuelle.PositionY = ParticuleActuelle.PositionY + math.Sin(angle1)*ParticuleActuelle.ScaleY*0.1
 					ParticuleProche.PositionX = ParticuleProche.PositionX + math.Cos(-angle1)*ParticuleProche.ScaleX*0.1
