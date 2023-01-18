3ccd2d09fd1fae5794707720e0eac20d430560e6
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2023-01-18 17:14:13 +0100

    MinAndMaxSpeed commentaires

--- particles/installationParticles/MinAndMaxSpeed_test.go
+++ particles/installationParticles/MinAndMaxSpeed_test.go
@@ -2,6 +2,9 @@ package installationParticles
 
 import "testing"
 
+// Le test TestSpeedModemoin1 vérifie que la fonction minAndMaxSpeed retourne bien 0 et 5
+// lorsque le mode de vitesse est -1
+// Si ce n'est pas le cas, le test échoue
 func TestSpeedModemoin1(t *testing.T) {
 	maxSpeed, minSpeed := minAndMaxSpeed(-1)
 	if maxSpeed != 5 || minSpeed != 0 {
@@ -9,6 +12,9 @@ func TestSpeedModemoin1(t *testing.T) {
 	}
 }
 
+// Le test TestSpeedMode1 vérifie que la fonction minAndMaxSpeed retourne bien 1 et 0
+// lorsque le mode de vitesse est 1
+// Si ce n'est pas le cas, le test échoue
 func TestSpeedMode1(t *testing.T) {
 	maxSpeed, minSpeed := minAndMaxSpeed(1)
 	if maxSpeed != 1 || minSpeed != 0 {
@@ -16,6 +22,9 @@ func TestSpeedMode1(t *testing.T) {
 	}
 }
 
+// Le test TestSpeeMode2 vérifie que la fonction minAndMaxSpeed retourne bien 3 et 1
+// lorsque le mode de vitesse est 2
+// Si ce n'est pas le cas, le test échoue
 func TestSpeeMode2(t *testing.T) {
 	maxSpeed, minSpeed := minAndMaxSpeed(2)
 	if maxSpeed != 3 || minSpeed != 1 {
@@ -23,6 +32,9 @@ func TestSpeeMode2(t *testing.T) {
 	}
 }
 
+// Le test TestSpeeMode3 vérifie que la fonction minAndMaxSpeed retourne bien 5 et 3
+// lorsque le mode de vitesse est 3
+// Si ce n'est pas le cas, le test échoue
 func TestSpeeMode3(t *testing.T) {
 	maxSpeed, minSpeed := minAndMaxSpeed(3)
 	if maxSpeed != 5 || minSpeed != 3 {
@@ -30,6 +42,9 @@ func TestSpeeMode3(t *testing.T) {
 	}
 }
 
+// Le test TestSpeeModeOther1 vérifie que la fonction minAndMaxSpeed retourne bien -1 et -1
+// lorsque le mode de vitesse est 10
+// Si ce n'est pas le cas, le test échoue
 func TestSpeeModeOther1(t *testing.T) {
 	maxSpeed, minSpeed := minAndMaxSpeed(10)
 	if maxSpeed != -1 || minSpeed != -1 {
@@ -37,6 +52,9 @@ func TestSpeeModeOther1(t *testing.T) {
 	}
 }
 
+// Le test TestSpeeModeOther2 vérifie que la fonction minAndMaxSpeed retourne bien -1 et -1
+// lorsque le mode de vitesse est 0
+// Si ce n'est pas le cas, le test échoue
 func TestSpeeModeOther2(t *testing.T) {
 	maxSpeed, minSpeed := minAndMaxSpeed(0)
 	if maxSpeed != -1 || minSpeed != -1 {
