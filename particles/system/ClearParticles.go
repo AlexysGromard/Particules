1234281245340fe00e2612286bbb6579383e7029
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2023-01-18 23:23:28 +0100

    Commentaires sur clearParticules

--- particles/system/ClearParticles.go
+++ particles/system/ClearParticles.go
@@ -1,5 +1,7 @@
 package system
 
+// La petite fonction ClearParticles permet de supprimer toutes les particules du système
+// Elle est appelée par le bouton Clear sur la page de configuration
 func (s *System) ClearParticles() {
 	NuméroPaticule := s.Content.Front()
 	for NuméroPaticule != nil {
