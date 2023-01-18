5c1b87378e0e10038955506e532e77e4fb932bd9
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2023-01-18 19:28:38 +0100

    Commentaires clearParticules

--- particles/system/ClearParticles_test.go
+++ particles/system/ClearParticles_test.go
@@ -6,16 +6,18 @@ import (
 	"testing"
 )
 
+// Le test TestClearParticles0 vérifie que toutes les particules vivantes du system sont tuées
 func TestClearParticles0(t *testing.T) {
+	// Création du system
 	Sys := System{Content: list.New(), DeadList: list.New()}
-
+	// Ajout de 100 particules vivantes au system
 	for NuméroToure := 0; NuméroToure < 100; NuméroToure++ {
 		Sys.Content.PushFront(Test.Basique_Particule())
 	}
-
+	// On tue toutes les particules vivantes du system
 	Sys.ClearParticles()
-
+	// On vérifie que toutes les particules vivantes du system ont été tuées
 	if Sys.Content.Len() != 0 {
-		t.Error("Tout les Particule Vivante du System n'on pas été tuer il reste ", Sys.Content.Len(), " Particule")
+		t.Error("Toutes les particules vivantes du system n'ont pas été tuées, il reste ", Sys.Content.Len(), " particule(s) vivante(s)")
 	}
 }
